6a95b5436d2f0084f1e29323fed4e02592d00c98
seddikHavre <seddik.tizit@gmail.com>
2017-04-16 21:41:29 +0200

    ceci est un commit

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,6 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
